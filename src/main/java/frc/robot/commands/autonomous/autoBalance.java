0963e7517ac51544a66a1d27afdf5dde8097a26d
Ezra Ju <96998749+EzraJJu@users.noreply.github.com>
2023-03-15 22:36:51 -0400

    auton commands add

---
+++ src/main/java/frc/robot/commands/autonomous/autoBalance.java
@@ -0,0 +1,7 @@
+package frc.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+
+public class autoBalance {
+    // this open source found here: https://github.com/FRC3683/OpenAutoBalance/blob/main/java/autoBalance.java
+}
---
+++ src/main/java/frc/robot/commands/autonomous/simple.java
@@ -0,0 +1,11 @@
+package frc.robot.commands.autonomous;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class simple extends CommandBase{
+    private DriveSubsystem drivetrain; 
+    private double travel; 
+
+}
