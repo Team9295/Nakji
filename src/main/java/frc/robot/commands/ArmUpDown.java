f668b2488bf60df70e14c6986c8fe0914de2010a
Mohith Manohar <32215646+mohith2017@users.noreply.github.com>
2023-03-20 18:17:06 -0400

    Update ArmUpDown.java

--- src/main/java/frc/robot/commands/ArmUpDown.java
+++ src/main/java/frc/robot/commands/ArmUpDown.java
@@ -1,5 +1,26 @@
 package frc.robot.commands;
 
-public class ArmUpDown {
-    
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ArmUpSubsystem;
+
+public class ArmUpDown extends CommandBase {
+    private final ArmUpSubsystem m_ArmUpSubsystem;
+    private final double m_speed;
+    private final double m_direction;
+
+    public ArmUpDown(ArmUpSubsystem ArmUpSubsystem, double speed, double direction) {
+        m_ArmUpSubsystem = ArmUpSubsystem;
+        m_speed = speed;
+        m_direction = direction;
+        addRequirements(m_ArmUpSubsystem);
+    }
+
+    public void initialize() {
+        m_ArmUpSubsystem.setSpeed(m_speed);
+        m_ArmUpSubsystem.setDirection(m_direction);
+    }
+
+    public void end(boolean interrupted) {
+        m_ArmUpSubsystem.setSpeed(0);
+    }
 }
