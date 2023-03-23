34a610defb7210dfadf9f49239507a04845b408c
Ezra Ju <96998749+EzraJJu@users.noreply.github.com>
2023-03-23 16:48:14 -0400

    typo?

--- src/main/java/frc/robot/commands/WristRotateCommand.java
+++ src/main/java/frc/robot/commands/WristRotateCommand.java
@@ -3,11 +3,11 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.WristRotationSubsystem;
 
-public class WristRoatate extends CommandBase{
+public class WristRotateCommand extends CommandBase{
     private final WristRotationSubsystem m_WristRotationSubsystem;
     private final double m_speed;
     
-    public WristRoatate(WristRotationSubsystem wristRotationSubsystem, double speed) {
+    public WristRotateCommand(WristRotationSubsystem wristRotationSubsystem, double speed) {
         m_WristRotationSubsystem = wristRotationSubsystem; 
         m_speed = speed; 
         addRequirements(m_WristRotationSubsystem);
