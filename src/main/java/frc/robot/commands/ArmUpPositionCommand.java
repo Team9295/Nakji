a378d700972cf54e95d8143261cacc59db09993e
yemily17 <emilyyin2020@gmail.com>
2023-03-29 17:33:52 -0400

    Level commands

---
+++ src/main/java/frc/robot/commands/ArmUpPositionCommand.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+import javax.swing.text.Position;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ArmUpSubsystem;
+
+public class ArmUpPositionCommand extends CommandBase{
+    private final ArmUpSubsystem m_armUpSubsystem;
+    private final double Position;
+
+    public ArmUpPositionCommand(ArmUpSubsystem armUpSubsystem, double Position) {
+        m_armUpSubsystem = armUpSubsystem;
+        m_Position = Position;
+        addRequirements(m_armUpSubsystem);
+    }
+
+    public void execute() {
+        m_armUpSubsystem.setPosition(m_Position);
+    }
+}
