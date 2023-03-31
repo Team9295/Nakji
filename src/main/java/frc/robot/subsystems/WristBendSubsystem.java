5d1d1a46067fa9acd418a7c0f4ca2053baf77bd5
Zaureau15 <56055012+Zaureau15@users.noreply.github.com>
2023-03-31 19:41:51 -0400

    Added servo1 and servo2 objects and set limits limits are opposite of each other

--- src/main/java/frc/robot/subsystems/WristBendSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristBendSubsystem.java
@@ -6,9 +6,13 @@ import frc.robot.Constants.WristBendConstants;
 
 public class WristBendSubsystem extends SubsystemBase {
   private final Servo m_motor = new Servo(WristBendConstants.kWristBendChannel);
+  private final Servo m_Servo1 = new Servo(WristBendConstants.kWristServo1Channel);
+  private final Servo m_Servo2 = new Servo(WristBendConstants.kWristServo2Channel);
 
   public WristBendSubsystem() {
     m_motor.setBounds(1.95, 0, 1.5, 0, 1.05);
+    m_Servo1.setBounds(2.3, 0, 1.5, 0, 0.7);
+    m_Servo2.setBounds(.7, 0, 1.5, 0, 2.3);
   }
     public void periodic() {
     }
